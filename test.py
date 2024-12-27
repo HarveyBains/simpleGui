fb0ad7024d0119adc45204a56f0299a2a59755c0
harveybains <9095-harveybains@users.noreply.replit.com>
2024-12-27 18:00:11 +0000

    .

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 import pygame
 
-# Initialize the mixer
+# Initialize the pygame mixer
 pygame.mixer.init()
-
-# Load the .wav file
 sound = pygame.mixer.Sound("339129__indigoray__beep-select.wav")
-
-# Play the sound
 sound.play()
-
-# Wait to allow the sound to finish (optional)
 pygame.time.wait(1000)  # Adjust based on sound duration
