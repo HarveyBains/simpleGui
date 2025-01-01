93fe3d6fbb4fbf61241efc1833dcded94e3d8dd5
Harvey Bains <HarveyBains146@gmail.com>
2025-01-01 17:09:44 +0000

    Handle muti paras

--- BlockMedition.py
+++ BlockMedition.py
@@ -1,6 +1,6 @@
 """
-    Version 3.2
-    DateTime:27/12/24 17:14
+    Version 1.0
+    DateTime:01/01/25 17:09
 """
 
 import random
@@ -10,7 +10,7 @@ import PySimpleGUI as sg
 pygame.mixer.init()
 
 myParagraph1:str = "Your present beliefs govern the actualisation of events.\nCreativity and experience is being created by each and every individual.\nYour present is where flesh meets with matter and spirit.\nTherefore your present is the point of power"
-myParagraph2:str = "Your clothes govern the actualisation of events.\nCreativity and experience is being created by each and every individual.\nYour present is where flesh meets with matter and spirit.\nTherefore your present is the point of power"
+myParagraph2:str = "Your xxx govern the actualisation of events.\nCreativity xx experience is being created by each and every individual.\nYourxxxx present is where flesh meets with matter and spirit.\nxx your present is the point of xxxx"
 
 
 
@@ -18,9 +18,9 @@ def create_window(theme):
     sg.theme(theme)
     layout = [
         [sg.Text("Meditation Labels", font=("Helvetica", 14, "bold"))],
-        [sg.Multiline(default_text=myParagraph1, key="inp_csvData", size=(50, 7), font=("Helvetica", 8, "bold"), background_color="light grey", text_color="black")],
+        [sg.Multiline(default_text=myParagraph1, key="inp_txtBlock", size=(50, 7), font=("Helvetica", 8, "bold"), background_color="light grey", text_color="black")],
         [sg.Text("", key="txt_Selected", font=("Helvetica", 6, "bold"))],
-        [sg.Button("Start", key="btn_Start", size=(4, 2)), sg.Button("Theme Toggle", key="tgl_Theme"), sg.Input("30, 1", key="inp_ParaNo", size=5, justification="center",font=("Helvetica", 8, "bold"), background_color="light grey")]
+        [sg.Button("Start", key="btn_Start", size=(4, 2)), sg.Button("Theme Toggle", key="tgl_Theme"), sg.Input("20,2", key="inp_ParaNo", size=5, justification="center",font=("Helvetica", 8, "bold"), background_color="light grey")]
     ]
     return sg.Window("My Meditation Gui", layout, size=(1000, 600))
 
@@ -29,13 +29,13 @@ def myFunction(inputStr: str):
     randomSel: int = random.randint(0, len(valuesList) - 1)
     return (f"{valuesList[randomSel].strip()}")
 
-def update_txtPara() -> str:
-    txtNo:str= values["inp_ParaNo"].split(",")[1]
-    if txtNo == "2":
-        return myParagraph2
-    else:
-        return myParagraph1
 
+def update_txtPara(txtNo: str) -> str:
+    match txtNo:
+        case "1": return myParagraph1
+        case "2": return myParagraph2
+        case _: return "Invalid paragraph number"
+    
 
 
 theme = "DarkBlue3"  # Default theme
@@ -45,21 +45,21 @@ window = create_window(theme)
 # Event loop: Keep the window open and responsive
 while True:
     event, values = window.read(timeout=100)  # Read user interactions
-
     if event == "btn_Start":
-        
-        # update the textParaNo
-        window["inp_csvData"].update(update_txtPara(), values)
+
+        # update the txtParagraph input field
+        txtParaNo:str = values["inp_ParaNo"].split(",")[1]
+        window["inp_txtBlock"].update(update_txtPara(txtParaNo))
 
       
         # Update the text field with new content
-        sendStr: str = values["inp_csvData"]
+        sendStr: str = values["inp_txtBlock"]
         window["txt_Selected"].update(myFunction(sendStr))
         window["btn_Start"].update(disabled=True)
 
 
         # Run the delay timer
-        timerDelay = int(values["inp_ParaNo"].split(",")[0])
+        timerDelay:int = int(values["inp_ParaNo"].split(",")[0])
         for seconds in range(timerDelay, 0, -1):
             window.read(timeout=1000)
         window["btn_Start"].update(disabled=False)
@@ -71,10 +71,9 @@ while True:
 
     if event == "tgl_Theme":
         # Change theme
-        if theme == 'DarkBlue3':
-            theme = 'DarkBlue'
-        else:
-            theme = 'DarkBlue3'
+        if theme == 'DarkBlue3': theme = 'DarkBlue'
+        else: theme = 'DarkBlue3'
+        
         window.close()
         window = create_window(theme)
 
