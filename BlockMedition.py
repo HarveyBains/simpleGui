d4102ba9ad3490f3a5ea4059651c2b9cac75ebdd
Harvey Bains <HarveyBains146@gmail.com>
2025-01-01 14:52:05 +0000

    more text size adjustments

--- BlockMedition.py
+++ BlockMedition.py
@@ -14,9 +14,9 @@ def create_window(theme):
     layout = [
         [sg.Text("Meditation Labels", font=("Helvetica", 14, "bold"))],
         [sg.Multiline("5.\nYour present beliefs govern the actualisation of events.\nCreativity and experience is being created by each and every individual.\nwhere flesh meets with matter and spirit.\nTherefore your present is the point of power",
-            key="inp_csvData", size=(50, 8), font=("Helvetica", 10, "bold"), background_color="light grey", text_color="black"
+            key="inp_csvData", size=(50, 8), font=("Helvetica", 8, "bold"), background_color="light grey", text_color="black"
             )],
-        [sg.Text("", key="txt_Selected", font=("Helvetica", 10, "bold"))],
+        [sg.Text("", key="txt_Selected", font=("Helvetica", 8, "bold"))],
         [sg.Button("Start", key="btn_Start", size=(4, 2)), sg.Button("Theme Toggle", key="tgl_Theme")]
     ]
     return sg.Window("My Meditation Gui", layout, size=(600, 900))
